9109dfda7702c58386ee6c5dea0c4a5287d95cee
yewwey <89197996+yewwey@users.noreply.github.com>
2022-08-27 19:23:36 +0900

    Update ProfileActivity.java
    Add profile image

--- app/src/main/java/com/yejija/myapplication/ProfileActivity.java
+++ app/src/main/java/com/yejija/myapplication/ProfileActivity.java
@@ -1,17 +1,56 @@
 package com.yejija.myapplication;
 
+import java.io.File;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
-    private static final String TAG = "ProfileActivity";
+        private final int GET_GALLERY_IMAGE = 200;
+        private ImageView imageview;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
 
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_profile);
 
+            final Button button = findViewById(R.id.btn_UploadPicture);
+            imageview = (ImageView)findViewById(R.id.user_image);
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                    startActivityForResult(intent, GET_GALLERY_IMAGE);
+                }
+            });
+        }
+
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+                Uri selectedImageUri = data.getData();
+                imageview.setImageURI(selectedImageUri);
+
+            }
+
+        }
 }
